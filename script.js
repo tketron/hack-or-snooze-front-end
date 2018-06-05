251814e1fec3c675afa8d8e35a6785326bd872a3
Tyler Ketron <tketron@gmail.com>
2018-06-04 20:15:48 -0700

    implemented favorites feature

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+let $links;
+
 $(function() {
   $('#submitText').on('click', function() {
     $('.form').slideToggle();
   });
+  $('#favoritesText').on('click', toggleFavorites);
+  $links = $('.link-row');
   $('.container').on('submit', '#submitForm', function(event) {
     event.preventDefault();
     addNewLink();
   });
 
-  $('.star').on('click', toggleFavorite);
+  $('.star').on('click', toggleFavoriteIcon);
 });
 
 function addNewLink() {
@@ -20,17 +24,33 @@ function addNewLink() {
     `<li class="link-row"><i class="star far fa-star"></i><a href=${url}>${title}<span class="link">(${url})</span></a></li>`
   );
   $('#links').append($newLink);
+  $links = $('.link-row');
 
-  $('.star').on('click', toggleFavorite);
+  $('.star').on('click', toggleFavoriteIcon);
+
   //clear form
   $('#title').val('');
   $('#url').val('');
 }
 
-function toggleFavorite() {
+function toggleFavoriteIcon() {
   console.log(event.target);
   $(event.target).toggleClass('far fas');
   $(event.target)
     .parent()
     .toggleClass('favorite');
+}
+
+function toggleFavorites() {
+  if ($('#links').hasClass('all')) {
+    let $favorites = $('.link-row').filter('.favorite');
+    $('#links').empty();
+    $('#links').append($favorites);
+    $('#favoritesText').text('all');
+  } else {}
+    $('#links').empty();
+    $('#links').append($links);
+    $('#favoritesText').text('favorites');
+  }
+  $('#links').toggleClass('all favorites');
 }
